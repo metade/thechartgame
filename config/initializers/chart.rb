ec0834006184486566a5bbe17156019095a5b667
Patrick Sinclair <metade@livro.config>
2009-09-11 11:21:31 +0100

    Fetching data from web if unable to find charts file

--- config/initializers/chart.rb
+++ config/initializers/chart.rb
@@ -4,4 +4,9 @@ chart_path = (RAILS_ENV=='test') ?
   File.join(RAILS_ROOT, 'spec', 'fixtures', 'chart.yml') :
   File.join(RAILS_ROOT, 'tmp', 'chart.yml')
 
-CHART = YAML.load(File.read(chart_path))
+begin
+  CHART = YAML.load(File.read(chart_path))
+rescue
+  doc = Hpricot(open('http://www.bbc.co.uk/radio1/chart/singles.shtml'))
+  CHART = (doc/'//td/h4').map { |e| e.inner_html }
+end  
