df63eb72521feff38b4f6bbee3a28b681391fba6
Patrick Sinclair <metade@livro.config>
2009-09-16 00:37:48 +0100

    Removed manual input of predictions

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -3,19 +3,6 @@ class PredictionsController < ApplicationController
     @predictions = Prediction.find(:all)
   end
   
-  def new
-    @prediction = Prediction.new(:user => User.new)
-  end
-  
-  def create
-    @prediction = Prediction.new(params[:prediction])
-    if @prediction.save
-      redirect_to predictions_path
-    else
-      render :action => "new"
-    end
-  end
-  
   def fetch
     Prediction.fetch_from_twitter
     chart = Chart.update_chart
