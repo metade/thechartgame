b43f78a53b515c372fd1fa312e16a72dcb415fc3
Patrick Sinclair <metade@livro.config>
2009-09-16 00:56:20 +0100

    Using text for chart variables

--- db/migrate/20090911110128_create_charts.rb
+++ db/migrate/20090911110128_create_charts.rb
@@ -1,8 +1,8 @@
 class CreateCharts < ActiveRecord::Migration
   def self.up
     create_table :charts do |t|
-      t.string :albums
-      t.string :singles
+      t.text :albums
+      t.text :singles
       
       t.integer :year
       t.integer :week
